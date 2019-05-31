6e2754d2ffe6600069e4f2531941c469e1984b57
olivier st-laurent <kzaurckichz@gmail.com>
2019-05-31 00:03:01 -0400

    added empty modules

---
+++ Vulkan4D.h
@@ -0,0 +1,35 @@
+// Vulkan4D Core Header
+
+#pragma once
+
+
+
+//////////////////////////////////////////////////////////
+// HELPERS
+
+#include "helpers/time.hpp"
+#include "helpers/log.hpp"
+#include "helpers/debug.hpp"
+#include "helpers/thread.hpp"
+#include "helpers/file.hpp"
+#include "helpers/stream.hpp"
+
+
+//////////////////////////////////////////////////////////
+// CORE MODULES
+
+#include "modules/Rendering.h"
+#include "modules/Audio.h"
+#include "modules/Input.h"
+#include "modules/Physics.h"
+#include "modules/Networking.h"
+#include "modules/FileSystem.h"
+
+
+//////////////////////////////////////////////////////////
+// MACROS
+
+// dynamic system loader
+//...
+
+
