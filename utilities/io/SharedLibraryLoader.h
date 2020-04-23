8428553a03d2c82b12068c459274638a9faae427
olivier st-laurent <kzaurckichz@gmail.com>
2020-04-23 02:59:36 -0400

    ..

--- utilities/io/SharedLibraryLoader.h
+++ utilities/io/SharedLibraryLoader.h
@@ -21,7 +21,7 @@ namespace v4d::io {
 	class V4DLIB SharedLibraryLoader {
 	private:
 		std::mutex loadedLibrariesMutex;
-		std::unordered_map<std::string, SharedLibraryInstance*> loadedLibraries;
+		std::unordered_map<std::string, SharedLibraryInstance*> loadedLibraries {};
 
 	public:
 	
