ba791d13d4a488308e9db0ed2f04399c27de656b
olivier st-laurent <kzaurckichz@gmail.com>
2020-05-14 03:55:49 -0400

    debug ui also in release mode for now

--- modules/V4D_Game.h
+++ modules/V4D_Game.h
@@ -2,11 +2,11 @@
 #include <v4d.h>
 
 #ifdef _ENABLE_IMGUI
-	#ifdef _DEBUG
+	// #ifdef _DEBUG
 		#define __V4D_Game_IMGUI ,RunImGui, RunImGuiDebug
-	#else
-		#define __V4D_Game_IMGUI ,RunImGui
-	#endif
+	// #else
+		// #define __V4D_Game_IMGUI ,RunImGui
+	// #endif
 #else
 	#define __V4D_Game_IMGUI
 #endif
@@ -32,9 +32,9 @@ class V4DLIB V4D_Game {
 	V4D_MODULE_FUNC(void, DestroyResources, v4d::graphics::vulkan::Device*)
 	#ifdef _ENABLE_IMGUI
 		V4D_MODULE_FUNC(void, RunImGui)
-		#ifdef _DEBUG
+		// #ifdef _DEBUG
 			V4D_MODULE_FUNC(void, RunImGuiDebug)
-		#endif
+		// #endif
 	#endif
 	V4D_MODULE_FUNC(void, Update, v4d::graphics::Scene&)
 	V4D_MODULE_FUNC(void, Update2, v4d::graphics::Scene&)
