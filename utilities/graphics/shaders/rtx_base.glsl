bc22679aea1361f1f0786d72d05d1fcfdad0c76d
olivier st-laurent <kzaurckichz@gmail.com>
2020-05-22 00:23:43 -0400

    WIP refactoring renderer

--- utilities/graphics/shaders/rtx_base.glsl
+++ utilities/graphics/shaders/rtx_base.glsl
@@ -1,10 +1,9 @@
-#extension GL_EXT_ray_tracing : enable
-#extension GL_EXT_nonuniform_qualifier : enable
-#extension GL_EXT_buffer_reference : enable
-
 #if defined(SHADER_RGEN) || defined(SHADER_RMISS) || defined(SHADER_RCHIT) || defined(SHADER_RAHIT) || defined(SHADER_RINT)
 	#define RAY_TRACING
-		
+	#extension GL_EXT_ray_tracing : enable
+	#extension GL_EXT_nonuniform_qualifier : enable
+	#extension GL_EXT_buffer_reference : enable
+
 	// Ray Tracing Payload
 	struct RayPayload_visibility {
 		uint geometryIndex;
